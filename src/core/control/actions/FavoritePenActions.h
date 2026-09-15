b154ca9cc0e9234c4969a62f90f1eade4a217b1a
fanlaoshi027 <fanzeping@gmail.com>
2026-09-15 13:58:49 +0800

    Use native GVariant int for favorite pen slot

--- src/core/control/actions/FavoritePenActions.h
+++ src/core/control/actions/FavoritePenActions.h
@@ -3,7 +3,6 @@
 #include "control/FavoritePenManager.h"
 #include "control/Control.h"
 #include "enums/Action.enum.h"
-#include "util/GVariantTemplate.h"
 
 /**
  * 墨写收藏笔槽：toolbar.ini 可用 FAVORITE_PEN(0..4) 直接调用。
@@ -13,7 +12,7 @@ struct ActionProperties<Action::FAVORITE_PEN> {
     using parameter_type = int;
 
     static void callback(GSimpleAction*, GVariant* parameter, Control* ctrl) {
-        const int slot = getGVariantValue<int>(parameter);
+        const int slot = g_variant_get_int32(parameter);
         FavoritePenManager::apply(ctrl->getToolHandler(), slot);
     }
 };
