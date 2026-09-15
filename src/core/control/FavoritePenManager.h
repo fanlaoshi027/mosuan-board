7056eaf5773c809df6c2923ddbe4110e9bb0135a
fanlaoshi027 <fanzeping@gmail.com>
2026-09-15 15:30:10 +0800

    Persist 墨写 favorite pen presets

--- src/core/control/FavoritePenManager.h
+++ src/core/control/FavoritePenManager.h
@@ -9,6 +9,7 @@
 #include "control/ToolEnums.h"
 #include "util/Color.h"
 
+class Settings;
 class ToolHandler;
 
 auto constexpr FAVORITE_PEN_COUNT = 5;
@@ -21,8 +22,9 @@ struct FavoritePenPreset {
 
 class FavoritePenManager {
 public:
+    static void load(Settings* settings);
     static void apply(ToolHandler* toolHandler, int slot);
-    static void capture(ToolHandler* toolHandler, int slot);
+    static void capture(ToolHandler* toolHandler, Settings* settings, int slot);
     static const FavoritePenPreset& get(int slot);
 
 private:
