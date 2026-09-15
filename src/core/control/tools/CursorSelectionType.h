c72459b74ee4eebf7af794791630331dd5556b06
fanlaoshi027 <fanzeping@gmail.com>
2026-09-15 18:09:50 +0800

    Add vertex selection cursor type

--- src/core/control/tools/CursorSelectionType.h
+++ src/core/control/tools/CursorSelectionType.h
@@ -16,6 +16,9 @@ enum CursorSelectionType {
     // Inside the selection
     CURSOR_SELECTION_MOVE = 1,
 
+    // Geometry vertex editing
+    CURSOR_SELECTION_VERTEX,
+
     // Edges
     CURSOR_SELECTION_TOP_LEFT,
     CURSOR_SELECTION_TOP_RIGHT,
