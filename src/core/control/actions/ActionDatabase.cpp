f158434f9819f2f006eee6f133c95f6c765bb2a2
fanlaoshi027 <fanzeping@gmail.com>
2026-09-15 13:57:52 +0800

    Wire favorite pen action into action database

--- src/core/control/actions/ActionDatabase.cpp
+++ src/core/control/actions/ActionDatabase.cpp
@@ -11,6 +11,7 @@
 #include "util/safe_casts.h"  // for to_underlying Todo(cpp20) remove
 
 #include "ActionProperties.h"
+#include "FavoritePenActions.h"
 
 #ifdef DEBUG_ACTION_DB
 #define START_ROW "   * " << std::left << std::setw(30) << std::boolalpha << Action_toString(a)
@@ -27,9 +28,7 @@ struct InitiallyEnabled<a, std::void_t<decltype(&ActionProperties<a>::initiallyE
     }
 };
 
-
 class ActionDatabase::Populator {
-    /// Choose the right action namespace
     template <Action a, class U = void>
     struct ActionNamespace {
         static constexpr auto ACTION_NAMESPACE = "win.";
@@ -38,8 +37,7 @@ class ActionDatabase::Populator {
         }
     };
     template <Action a>
-    struct ActionNamespace<
-            a, std::enable_if_t<std::is_same_v<typename ActionProperties<a>::app_namespace, std::true_type>, void>> {
+    struct ActionNamespace<a, std::enable_if_t<std::is_same_v<typename ActionProperties<a>::app_namespace, std::true_type>, void>> {
         static constexpr auto ACTION_NAMESPACE = "app.";
         static void addToActionMap(ActionDatabase* db) {
             g_action_map_add_action(G_ACTION_MAP(gtk_window_get_application(GTK_WINDOW(db->win))),
@@ -47,9 +45,6 @@ class ActionDatabase::Populator {
         }
     };
 
-    /**
-     * Accelerators<a>::setup(ctrl); will setup the accelerators listed in ActionProperties<a>::accelerators (if any)
-     */
     template <Action a, class U = void>
     struct Accelerators {
         static inline void setup(Control*) {}
@@ -57,15 +52,11 @@ class ActionDatabase::Populator {
     template <Action a>
     struct Accelerators<a, std::void_t<decltype(&ActionProperties<a>::accelerators)>> {
         static_assert(std::is_array_v<decltype(ActionProperties<a>::accelerators)>);
-        static_assert(
-                std::is_same_v<std::remove_extent_t<decltype(ActionProperties<a>::accelerators)>, const char* const>);
-        static_assert(
-                ActionProperties<a>::accelerators[std::extent_v<decltype(ActionProperties<a>::accelerators)> - 1] ==
-                        nullptr,
-                "ActionProperties<a>::accelerators must be null terminated");
+        static_assert(std::is_same_v<std::remove_extent_t<decltype(ActionProperties<a>::accelerators)>, const char* const>);
+        static_assert(ActionProperties<a>::accelerators[std::extent_v<decltype(ActionProperties<a>::accelerators)> - 1] == nullptr,
+                      "ActionProperties<a>::accelerators must be null terminated");
 
         static inline void setup(Control* ctrl) {
-            // Todo(cpp20) constexpr this concatenation
             std::string fullActionName = ActionNamespace<a>::ACTION_NAMESPACE;
             fullActionName += Action_toString(a);
             gtk_application_set_accels_for_action(GTK_APPLICATION(gtk_window_get_application(ctrl->getGtkWindow())),
@@ -78,71 +69,53 @@ class ActionDatabase::Populator {
         db->signalIds[a] = g_signal_connect(G_OBJECT(db->gActions[a].get()), signal,
                                             G_CALLBACK(ActionProperties<a>::callback), db->control);
         ActionNamespace<a>::addToActionMap(db);
-        // g_action_map_add_action(G_ACTION_MAP(db->win), G_ACTION(db->gActions[a].get()));
         Accelerators<a>::setup(db->control);
         InitiallyEnabled<a>::setup(db, db->control);
     }
 
-    // Actions without state or parameter
     template <Action a, std::enable_if_t<!has_param<a>() && !has_state<a>(), bool> = true>
     static void assign(ActionDatabase* db) {
-
         ACTIONDB_PRINT_DEBUG(START_ROW << " |               |            |");
-
         db->gActions[a].reset(g_simple_action_new(Action_toString(a), nullptr), xoj::util::adopt);
-
         finishSetup<a>(db, "activate");
     }
 
-    // Actions with parameter but no state
     template <Action a, std::enable_if_t<has_param<a>() && !has_state<a>(), bool> = true>
     static void assign(ActionDatabase* db) {
-
         ACTIONDB_PRINT_DEBUG(START_ROW << " |               | type = \""
                                        << (const char*)gVariantType<typename ActionProperties<a>::parameter_type>()
                                        << "\" |");
-
         db->gActions[a].reset(
                 g_simple_action_new(Action_toString(a), gVariantType<typename ActionProperties<a>::parameter_type>()),
                 xoj::util::adopt);
-
         finishSetup<a>(db, "activate");
     }
 
-    // Actions with a state but no parameter
     template <Action a, std::enable_if_t<!has_param<a>() && has_state<a>(), bool> = true>
     static void assign(ActionDatabase* db) {
-
         ACTIONDB_PRINT_DEBUG(START_ROW << " | \""
                                        << (const char*)gVariantType<typename ActionProperties<a>::state_type>()
                                        << "\" = " << std::setw(7) << ActionProperties<a>::initialState(db->control)
                                        << " |            |");
-
         db->gActions[a].reset(g_simple_action_new_stateful(Action_toString(a), nullptr,
                                                            makeGVariant<typename ActionProperties<a>::state_type>(
                                                                    ActionProperties<a>::initialState(db->control))),
                               xoj::util::adopt);
-
         finishSetup<a>(db, "change-state");
     }
 
-    // Actions with both state and parameter (with matching type)
     template <Action a, std::enable_if_t<has_param<a>() && has_state<a>(), bool> = true>
     static void assign(ActionDatabase* db) {
-        static_assert(
-                std::is_same_v<typename ActionProperties<a>::state_type, typename ActionProperties<a>::parameter_type>);
-
-        ACTIONDB_PRINT_DEBUG(
-                START_ROW << " | \"" << (const char*)gVariantType<typename ActionProperties<a>::state_type>()
-                          << "\" = " << std::setw(7) << ActionProperties<a>::initialState(db->control) << " | type = \""
-                          << (const char*)gVariantType<typename ActionProperties<a>::state_type>() << "\" |");
-
+        static_assert(std::is_same_v<typename ActionProperties<a>::state_type, typename ActionProperties<a>::parameter_type>);
+        ACTIONDB_PRINT_DEBUG(START_ROW << " | \""
+                                       << (const char*)gVariantType<typename ActionProperties<a>::state_type>()
+                                       << "\" = " << std::setw(7) << ActionProperties<a>::initialState(db->control) << " | type = \""
+                                       << (const char*)gVariantType<typename ActionProperties<a>::state_type>() << "\" |");
         db->gActions[a].reset(g_simple_action_new_stateful(Action_toString(a),
                                                            gVariantType<typename ActionProperties<a>::state_type>(),
                                                            makeGVariant<typename ActionProperties<a>::state_type>(
                                                                    ActionProperties<a>::initialState(db->control))),
                               xoj::util::adopt);
-
         finishSetup<a>(db, "change-state");
     }
 
@@ -151,7 +124,6 @@ class ActionDatabase::Populator {
         ((assign<static_cast<Action>(As)>(db)), ...);
     }
 
-
 public:
     static void populate(ActionDatabase* db);
 };
@@ -159,7 +131,6 @@ public:
 void ActionDatabase::Populator::populate(ActionDatabase* db) {
     ACTIONDB_PRINT_DEBUG("Populating ActionDatabase:");
     ACTIONDB_PRINT_DEBUG("        ACTION NAME:                |  STATE INIT   | PARAM TYPE |");
-
     populateImpl(std::make_index_sequence<xoj::to_underlying(Action::ENUMERATOR_COUNT)>(), db);
 }
 
@@ -171,14 +142,9 @@ ActionDatabase::ActionDatabase(Control* control):
 ActionDatabase::~ActionDatabase() {
     auto sig = signalIds.begin();
     for (auto& a: gActions) {
-        /**
-         * The GAction's might still be referenced elsewhere (e.g. by the GtkApplicationWindow), so we need to
-         * disconnect the signals in case a callback is called and the Control instance has been destroyed
-         */
         g_signal_handler_disconnect(a.get(), *sig++);
     }
 }
-
 
 void ActionDatabase::enableAction(Action action, bool enable) {
     xoj_assert(gActions[action]);
